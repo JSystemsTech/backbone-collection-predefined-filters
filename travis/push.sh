8cf428ea016aa35de54a216627bc7671eceb1fb1
JSystemsTech <jsystemstech@gmail.com>
2016-10-12 22:47:06 -0400

    release1.0.0: update travis/push.sh

--- travis/push.sh
+++ travis/push.sh
@@ -1,21 +1,19 @@
 #!/bin/sh
 
-if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+if [ "$TRAVIS_PULL_REQUEST" == "false" ]
+then
   echo -e "Starting to update README.md\n"
 
-  #copy data we're interested in to other place
-  cp -R coverage $HOME/coverage
-
-  #go to home and setup git
-  cd $HOME
+  #cd to home and setup git
+  cd "$HOME" || exit
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis"
 
   #using token clone master branch
-  git clone --quiet --branch=master https://${GH_TOKEN}@github.com/JSystemsTech/backbone-collection-predefined-filters.git  master > /dev/null
+  git clone --quiet --branch=master https://"${GH_TOKEN}"@github.com/JSystemsTech/backbone-collection-predefined-filters.git  master > /dev/null
 
-  #go into diractory and copy data we're interested in to that directory
-  
+  #cd into master
+  cd master || exit
   git add -f . README.md
   git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed README.md update to master"
   git push -fqu origin master > /dev/null
