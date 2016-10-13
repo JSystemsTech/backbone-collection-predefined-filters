e1a102fa00c01f6a42397bdafc98d49cb77abbab
JSystemsTech <jsystemstech@gmail.com>
2016-10-13 00:25:31 -0400

    release1.0.0: add git checkout master and merge detached head in travis ci

--- travis/push.sh
+++ travis/push.sh
@@ -8,11 +8,13 @@ then
   git config --global user.name "Travis"
  
   git branch
+
   echo -e "git add README.md\n"
   git add README.md
   echo -e "git commit\n"
   git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed README.md update to master"
-
+  git checkout master
+  git merge HEAD@{1}
   echo -e "push to master\n"
   git remote add origin-master https://"${GH_TOKEN}"@github.com/JSystemsTech/backbone-collection-predefined-filters.git > /dev/null 2>&1
   git push --quiet --set-upstream origin-master master
