796f905ffb392bc6cf39c61d8578764124c60367
JSystemsTech <jsystemstech@gmail.com>
2016-10-12 21:44:11 -0400

    release1.0.0: debug travis ci git push

--- .travis/push.sh
+++ .travis/push.sh
@@ -12,6 +12,7 @@ commit_website_files() {
 }
 
 upload_files() {
+  echo git remote add origin-master https://${GH_TOKEN}@github.com/JSystemsTech/backbone-collection-predefined-filters.git > /dev/null 2>&1
   git remote add origin-master https://${GH_TOKEN}@github.com/JSystemsTech/backbone-collection-predefined-filters.git > /dev/null 2>&1
   git push --quiet --set-upstream origin-master master 
 }
