e76534631683f2a54725a4f97353848c227e8b0c
JSystemsTech <jsystemstech@gmail.com>
2016-10-12 23:27:44 -0400

    release1.0.0: ad more print statements to push.sh to make it more debuggable in the travis ci pipeline

--- travis/push.sh
+++ travis/push.sh
@@ -3,18 +3,19 @@
 if [ "$TRAVIS_PULL_REQUEST" = "false" ]
 then
   echo -e "Starting to update README.md\n"
-
-  #cd to home and setup git
-  cd "$HOME" || exit
+  echo -e "Configure Git username and email\n"
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis"
 
+  echo -e "git checkout master\n"
   git checkout -b master
  
+  echo -e "git add README.md\n"
   git add -f . README.md
+  echo -e "git commit"
   git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed README.md update to master"
-  git push -fqu origin master > /dev/null
 
+  echo -e "push to master\n"
   git remote add origin-master https://"${GH_TOKEN}"@github.com/JSystemsTech/backbone-collection-predefined-filters.git > /dev/null 2>&1
   git push --quiet --set-upstream origin-master master
 
