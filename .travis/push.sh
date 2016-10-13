fd7fc42f649c48fc2a862193422030ab3704a80d
JSystemsTech <jsystemstech@gmail.com>
2016-10-12 22:23:14 -0400

    release1.0.0: refactor push.sh & update travis.yml

--- .travis/push.sh
+++ .travis/push.sh
@@ -1,22 +1,24 @@
 #!/bin/sh
 
-setup_git() {
-  git config --global user.email "jsystemstech@gmail.com"
-  git config --global user.name "Travis CI"
-}
+if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+  echo -e "Starting to update README.md\n"
 
-commit_website_files() {
-  git checkout -b master
-  git add . README.md
-  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER update README.md"
-}
+  #copy data we're interested in to other place
+  cp -R coverage $HOME/coverage
 
-upload_files() {
-  echo git remote add origin-master https://${GH_TOKEN}@github.com/JSystemsTech/backbone-collection-predefined-filters.git > /dev/null 2>&1
-  git remote add origin-master https://${GH_TOKEN}@github.com/JSystemsTech/backbone-collection-predefined-filters.git > /dev/null 2>&1
-  git push --quiet --set-upstream origin-master master 
-}
+  #go to home and setup git
+  cd $HOME
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis"
 
-setup_git
-commit_website_files
-upload_files
+  #using token clone master branch
+  git clone --quiet --branch=master https://${GH_TOKEN}@github.com/JSystemsTech/backbone-collection-predefined-filters.git  master > /dev/null
+
+  #go into diractory and copy data we're interested in to that directory
+  
+  git add -f . README.md
+  git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed README.md update to master"
+  git push -fqu origin master > /dev/null
+
+  echo -e "Pushed README.md update\n"
+fi
