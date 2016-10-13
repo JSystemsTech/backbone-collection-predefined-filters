f930d938e301847e669269e985a7da90ca3c41ee
JSystemsTech <jsystemstech@gmail.com>
2016-10-12 23:01:46 -0400

    release1.0.0: change to use branch checkout instead of clone

--- travis/push.sh
+++ travis/push.sh
@@ -9,14 +9,15 @@ then
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis"
 
-  #using token clone master branch
-  git clone --quiet --branch=master https://"${GH_TOKEN}"@github.com/JSystemsTech/backbone-collection-predefined-filters.git  master > /dev/null
-
-  #cd into master
   cd master || exit
+  git checkout -b master
+ 
   git add -f . README.md
   git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed README.md update to master"
   git push -fqu origin master > /dev/null
+
+  git remote add origin-master https://"${GH_TOKEN}"@github.com/JSystemsTech/backbone-collection-predefined-filters.git > /dev/null 2>&1
+  git push --quiet --set-upstream origin-master master
 
   echo -e "Pushed README.md update\n"
 fi
